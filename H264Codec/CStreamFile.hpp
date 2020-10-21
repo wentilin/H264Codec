e05e44f9dd504e9cd7ab1332785987de78a2c7e9
wentilin <837956177@qq.com>
2020-10-21 11:55:05 +0800

    添加descriptor说明

--- H264Codec/CStreamFile.hpp
+++ H264Codec/CStreamFile.hpp
@@ -13,6 +13,28 @@
 
 #include "Global.h"
 
+/**
+ 描述子(descriptor):
+    – ae(v): context-adaptive arithmetic entropy-coded syntax element.
+ 
+    – ce(v): context-adaptive variable-length entropy-coded syntax element with the left bit first.
+ 
+    – me(v): mapped Exp-Golomb-coded syntax element with the left bit first.
+ 
+    – se(v): signed integer Exp-Golomb-coded syntax element with the left bit first.
+ 
+    – te(v): truncated Exp-Golomb-coded syntax element with left bit first.
+ 
+    – b(8): byte having any pattern of bit string (8 bits). The parsing process for this descriptor is specified by the return value of the function    read_bits( 8 ).
+    
+    – f(n): fixed-pattern bit string using n bits written (from left to right) with the left bit first. The parsing process for this descriptor is specified by     the return value of the function read_bits( n ).
+    
+    – i(n): signed integer using n bits. When n is “v” in the syntax table, the number of bits varies in a manner dependent on the value of other   syntax elements. The parsing process for this descriptor is specified by the return value of the function read_bits( n ) interpreted as a two’s     complement integer representation with most significant bit written first.
+    
+    – u(n): unsigned integer using n bits. When n is “v” in the syntax table, the number of bits varies in a manner dependent on the value of   other syntax elements. The parsing process for this descriptor is specified by the return value of the function read_bits( n ) interpreted as a     binary representation of an unsigned integer with most significant bit written first.
+    
+    – ue(v): unsigned integer Exp-Golomb-coded syntax element with the left bit first.
+ */
 class CStreamFile {
 public:
     CStreamFile(char *fileName);
